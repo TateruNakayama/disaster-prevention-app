2902591551f0e0a338a728159476b45c8b380c63
TateruNakayama <Gr4pu3@gmail.com>
2024-02-06 21:40:08 +0900

    add: html&css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,22 +9,32 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css')
+
+    <link rel="icon" href="{{ asset('img/favicon.png') }}" />
+
+    <script>
+        (function(d) {
+            var config = {
+            kitId: 'hnx0kbj',
+            scriptTimeout: 3000,
+            async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document);
+    </script>
 </head>
 
 <body>
     <header>
-        <!-- 共通のヘッダー内容はここに記述 -->
         @include('layouts.header')
     </header>
 
     <main>
-        <!-- メインコンテンツはここに表示されます -->
         @yield('content')
     </main>
 
     <footer>
-        <!-- 共通のフッター内容はここに記述 -->
         @include('layouts.footer')
     </footer>
 </body>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.app')
 
-@section('title', 'トップページ')
+@section('title', '防災クイズ')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/start.css') }}" />
+@endsection
 
 @section('content')
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <p>防災クイズ</p>
+    <div class="header">
+        <img class="logo" src="{{ asset('img/logo.png') }}">
+    </div>
+
+    <div class="content">
+        <div class="explainbox">
+            <p>今から防災に関するクイズが１０問出題されます。
+                正解数に応じてプレゼントが変わるので全問正解目指して頑張ってください</p>
+        </div>
+    </div>
+    <div class="botton">
+        <button type="button" class="startbutton" onclick=" location.href='question.html'">START</button>
     </div>
 @endsection
